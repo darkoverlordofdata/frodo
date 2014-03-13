71a9f4a5899c389cfe00164c2a529fd7304586d5
darkoverlordofdata <darkoverlordofdata@gmail.com>
2014-03-12 19:23:52 -0700

    testing auth

--- server.js
+++ server.js
@@ -2,4 +2,36 @@ require('coffee-script');
 require('coffee-script/register');
 var muninn = require('muninn');
 muninn.init(__dirname);
+
+fb = require('fb');
+fb.options({
+    appId:          muninn.config.fb.appId,
+    appSecret:      muninn.config.fb.appSecret,
+    redirectUri:    muninn.config.fb.redirectUri
+});
+fb.authenticate = function($code, $next) {
+    return fb.api('oauth/access_token', {
+        client_id: fb.options('appId'),
+        client_secret: fb.options('appSecret'),
+        redirect_uri: fb.options('redirectUri'),
+        code: $code
+    }, function($result) {
+        if (!$result || $result.error) {
+            $next($result.error);
+        }
+        return fb.api('oauth/access_token', {
+            client_id: fb.options('appId'),
+            client_secret: fb.options('appSecret'),
+            grant_type: 'fb_exchange_token',
+            fb_exchange_token: $result.access_token
+        }, function($result) {
+            if (!$result || $result.error) {
+                return $next($result.error);
+            } else {
+                return $next(null, $result);
+            }
+        });
+    });
+};
+
 muninn.start();
